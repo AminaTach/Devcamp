4492c9d42897997380f01d40da70943239408fb1
AminaTach <121464221+AminaTach@users.noreply.github.com>
2025-04-17 21:27:17 +0100

    Initial commit

---
+++ devcamp/tailwind.config.js
@@ -0,0 +1,18 @@
+import flowbite from 'flowbite-react/tailwind';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+      'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
